de67b12b200357d674dc16ad36b9649b564302ba
SebastianJimenez2510 <sebastian.jimenezgarro@gmail.com>
2022-09-30 00:17:54 +0000

    Final push

--- src/app.js
+++ src/app.js
@@ -2,10 +2,20 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = [".com", ".net", ".io"];
+  var textoh1 = document.querySelector("#dgen");
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let aox = "";
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let index = 0; index < adj.length; index++) {
+      for (let ext = 0; ext < noun.length; ext++) {
+        console.log(pronoun[i] + " " + noun[ext]);
+        aox += "<p>" + pronoun[i] + adj[index] + noun[ext] + "</p>";
+      }
+    }
+    textoh1.innerHTML = aox;
+  }
 };
